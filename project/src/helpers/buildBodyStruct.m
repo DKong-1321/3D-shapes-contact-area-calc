c7e3f39372ad7c2731ff2bcab299f69c3cb26bb4
DKong-1321 <dk1321@ic.ac.uk>
2025-12-01 16:19:21 +0000

    Update to builderBodyStruct and main.m
    to accomodate for new code that shows the cylinder moving across the top of the cube and shown in a series of figures.
    The figures show cylinder sliding across the top of the cube, the contact area between the two shapes for each frame, and the change of contact area for each frame.

--- project/src/helpers/buildBodyStruct.m
+++ project/src/helpers/buildBodyStruct.m
@@ -8,6 +8,9 @@ function body = buildBodyStruct(F, V)
 %   triNormal  : per-face unit normal
 %   triArea    : per-face area
 %   bbox       : 3x2 [xmin xmax; ymin ymax; zmin zmax]
+%
+% Also creates alias fields:
+%   centroids, normals, areas
 
 body.F = F;
 body.V = V;
@@ -18,7 +21,9 @@ v2 = V(F(:,2),:);
 v3 = V(F(:,3),:);
 
 % Centroids
-body.triCentroid = (v1 + v2 + v3) / 3;
+triCentroid = (v1 + v2 + v3) / 3;
+body.triCentroid = triCentroid;
+body.centroids   = triCentroid;   % alias
 
 % Normals and areas
 e1 = v2 - v1;
@@ -29,9 +34,13 @@ area = 0.5 * sqrt(sum(n.^2,2));
 % Avoid division by zero
 nz = vecnorm(n,2,2);
 nz(nz==0) = 1;
-body.triNormal = n ./ nz;
+triNormal = n ./ nz;
+
+body.triNormal = triNormal;
+body.normals   = triNormal;       % alias
 
 body.triArea = area;
+body.areas   = area;              % alias
 
 % Bounding box
 xmin = min(V(:,1)); xmax = max(V(:,1));
